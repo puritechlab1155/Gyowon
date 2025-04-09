61fa31c691f3d7339e0adbe6d04587e37f3b3213
puritechlab1155 <pub@puritechlab.com>
2025-04-09 21:26:06 +0900

    사이드메뉴 수정

--- js/script.js
+++ js/script.js
@@ -1,37 +1,4 @@
-// const switchElement = document.getElementById('s1');
-// const switchContainer = document.querySelector('.switch');
-// const switchDot = document.querySelector('.dot');
-// const sideMenu = document.getElementById('side-menu');
-
-
-// // Toggle functionality for both checkbox and switch
-// function toggleSideMenu() {
-//     // Toggle checkbox state
-//     switchElement.checked = !switchElement.checked;
-
-//     // Update switch visual
-//     if (switchElement.checked) {
-//         switchContainer.classList.replace('bg-gray-300', 'bg-[#2B5BBB]');
-//         if (window.innerWidth <= 640) {
-//             switchDot.classList.add('translate-x-5'); // 작은 화면에서는 짧게 이동
-//         } else {
-//             switchDot.classList.add('translate-x-6'); // 기본 이동 거리
-//         }
-//         sideMenu.classList.remove('-translate-x-full');
-//         sideMenu.classList.add('translate-x-0');
-//     } else {
-//         switchContainer.classList.replace('bg-[#2B5BBB]', 'bg-gray-300');
-//         switchDot.classList.remove('translate-x-5', 'translate-x-6');
-//         sideMenu.classList.add('-translate-x-full');
-//         sideMenu.classList.remove('translate-x-0');
-//     }
-// }
-
-// // Add click event to the switch
-// switchContainer.addEventListener('click', toggleSideMenu);
-
-
-// // ✅ 화면 크기 변경 시 768px 이하일 때 메뉴 닫기
+// ✅ 화면 크기 변경 시 768px 이하일 때 메뉴 닫기
 // window.addEventListener('resize', () => {
 //     if (window.innerWidth <= 1024) {
 //         sideMenu.classList.add('-translate-x-full');
@@ -132,11 +99,11 @@ document.addEventListener('click', (event) => {
 // 서브 메뉴와 화살표 회전 토글 함수
 function toggleSubMenu(event) {
     // 클릭된 a 태그 요소의 부모 li 선택
-    const parentLi = event.target.closest('li');
+    const parentLi = event.target.closest('.group');
     
     // 서브 메뉴 토글
-    const submenu = parentLi.querySelector('ul');
-    const arrow = parentLi.querySelector('img');
+    const submenu = parentLi.querySelector('.subUl');
+    const arrow = parentLi.querySelector('.subIcon');
     
     // 서브 메뉴 열기/닫기
     submenu.classList.toggle('max-h-0');
@@ -151,18 +118,18 @@ function toggleSubMenu(event) {
 }
 
 // 서브 메뉴의 각 li 요소에 포커스 이벤트 리스너 추가
-document.querySelectorAll('#qualification-submenu li a').forEach(item => {
+document.querySelectorAll('#qualification-submenu .group .subLink').forEach(item => {
     item.addEventListener('focus', function() {
         // 현재 포커스된 li 요소의 부모 li 요소 선택
-        const parentLi = this.closest('li').parentElement.parentElement;
+        const parentLi = this.closest('.group').parentElement.parentElement;
         
         // 부모 li 요소에 포커스 스타일 적용 (hover 스타일과 동일하게 적용)
-        parentLi.querySelector('a').style.color = '#2B5BBB';
+        parentLi.querySelector('.subLink').style.color = '#2B5BBB';
     });
     
     item.addEventListener('blur', function() {
         // 포커스 해제 시 부모 li 요소의 포커스 스타일 제거
-        const parentLi = this.closest('li').parentElement.parentElement;
-        parentLi.querySelector('a').style.color = '';
+        const parentLi = this.closest('.group').parentElement.parentElement;
+        parentLi.querySelector('.subLink').style.color = '';
     });
 });
